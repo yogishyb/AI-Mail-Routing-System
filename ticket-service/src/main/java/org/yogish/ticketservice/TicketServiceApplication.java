fe4e61289f851059b537652028c989e3ec5f9f5c
yogishyb <yogishayb@gmail.com>
2025-11-29 20:37:42 +0530

    init

--- ticket-service/src/main/java/org/yogish/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/org/yogish/ticketservice/TicketServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TickerServiceApplication {
+public class TicketServiceApplication {
     public static void main(String[] args){
-        SpringApplication.run(TickerServiceApplication.class,args);
+        SpringApplication.run(TicketServiceApplication.class,args);
     }
 }
